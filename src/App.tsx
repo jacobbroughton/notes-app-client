f44f7968c0a55b8f22f768f377eb03e481005797
Jacob Broughton <jlbroughton88@gmail.com>
2023-04-02 00:22:56 -0400

    added safari warning browser

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Sidebar from "./components/ui/Sidebar/Sidebar";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import { AuthenticatedRoutes } from "./utils/AuthenticatedRoutes";
+import { detect } from "detect-browser";
+const browser = detect();
 
 function App() {
   const sidebar = useSelector((state: RootState) => state.sidebar);
@@ -17,6 +19,24 @@ function App() {
     localStorage.setItem("theme", theme);
     document.body.dataset.theme = theme;
   }, [theme]);
+
+  if (browser?.name === "safari")
+    return (
+      <div className="wrong-browser-view">
+        <div className="wrong-browser-container">
+          <h1>Oops!</h1>
+          <p>
+            It looks like you're using safari. Unforunately, the safari browser has disabled third-party
+            cookies from cross-origin domains. The only way around this is to purchase a yearly domain.
+          </p>
+          <p>
+            Since this is a portfolio project (not a for-profit app), I ask you to view
+            this on a non-webkit browser (like chrome) to ensure the optimal experience.
+          </p>
+          <strong>By the way, i'm sorry about this. Thanks in advance for checking out the project.</strong>
+        </div>
+      </div>
+    );
 
   return (
     <div className="App">
