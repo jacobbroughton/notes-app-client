5c5d2b12ac6553bcde1d9de363215954b63c56e2
Jacob Broughton <jlbroughton88@gmail.com>
2023-11-12 16:36:04 -0500

    changed api url

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -1,11 +1,11 @@
 export const getApiUrl = () => {
-  let API_URL
+  let API_URL;
 
   if (import.meta.env.PROD) {
-    API_URL = "https://notes-app-jb-8363f34d89d9.herokuapp.com/api"
+    API_URL = "https://notesjb.com/api";
   } else {
-    API_URL = "http://localhost:3001/api"
+    API_URL = "http://localhost:3001/api";
   }
 
-  return API_URL
-}
+  return API_URL;
+};
