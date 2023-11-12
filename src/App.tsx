9da0f6f3e5e3e5bc864a3a514bfbf73507b732c7
Jacob Broughton <jlbroughton88@gmail.com>
2023-11-12 16:01:26 -0500

    moved getUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Login from "./components/pages/Login/Login";
 import WrongBrowser from "./components/pages/WrongBrowser/WrongBrowser";
 import WindowTooSmall from "./components/pages/WindowTooSmall/WindowTooSmall";
 import Register from "./components/pages/Register/Register";
 import Sidebar from "./components/ui/Sidebar/Sidebar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import { AuthenticatedRoutes } from "./utils/AuthenticatedRoutes";
 import { detect } from "detect-browser";
 import useWindowSize from "./utils/useWindowSize";
+import { setUser } from "./redux/user";
+import { getApiUrl } from "./utils/getUrl";
 const browser = detect();
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
   const user = useSelector((state: RootState) => state.user);
   const theme = useSelector((state: RootState) => state.theme);
   const windowSize = useWindowSize();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
@@ -38,26 +42,43 @@ function App() {
   //   return <WindowTooSmall/>
   // }
 
-  let marginLeft = 0;
-  if (!user) {
-  }
-
   function determineMarginLeft(user: object | null) {
     if (!user) return 0;
-    if (sidebar.floating) return 45
+    if (sidebar.floating) return 45;
     return sidebar.toggled ? sidebar.width + 45 : 45;
   }
+
+  async function getUser() {
+    // setLoading(true)
+    const response = await fetch(`${getApiUrl()}/`, {
+      method: "GET",
+      credentials: "include",
+      headers: { "Access-Control-Allow-Origin": "http://localhost:3000" },
+    });
+
+    const data = await response.json();
+
+    if (!data.user) navigate("/login");
+    if (data.user && !user) {
+      dispatch(setUser(data.user));
+      // setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
 
   return (
     <div className="App">
       {/* // TODO - uncomment */}
       {user && <Sidebar />}
-      {/* <Sidebar /> */}
       <main
         style={{
           marginLeft: `${determineMarginLeft(user)}px`,
         }}
       >
+
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<AuthenticatedRoutes />}>
