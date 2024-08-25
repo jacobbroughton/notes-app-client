e53ff34d2f1018a626c2c9e51dfdcd7712d06825
Jacob Broughton <jlbroughton88@gmail.com>
2024-08-25 10:32:35 -0400

    added warning for long load time

--- src/components/ui/LoadingOverlay/LoadingOverlay.tsx
+++ src/components/ui/LoadingOverlay/LoadingOverlay.tsx
@@ -5,10 +5,15 @@ const LoadingOverlay = ({ message }: { message: string | undefined }) => {
   return (
     <div className="loading-overlay">
       <LoadingSpinner />
-      {message && <p>{message}</p>}
-      <p>
-        Loading may take longer than expected due to the server spinning down after not being used for a while.
-      </p>
+      {message && (
+        <div className="text">
+          <p>{message}</p>
+          <p className="long-time-text">
+            Loading may take longer than expected due to the server spinning down after
+            not being used for a while.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
